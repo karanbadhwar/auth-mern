0c184394dd401328e35d939ba254a97f913c3c17
karanbadhwar <karanbadhwar07@gmail.com>
2023-10-13 18:34:24 -0400

    express async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,9 @@
+import asyncHandler from "express-async-handler";
 // @desc Auth User/set token
 // route POST /api/users/auth
 //@access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ msg: "Auth User Route" });
-};
+});
 
 export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+
+//Other Packages
+import morgan from "morgan";
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 //Routers
 import userRoutes from "./routes/userRoutes.js";
+
+//Other Packages
+app.use(morgan("dev"));
 
 //Routes
 app.use("/api/users", userRoutes);
